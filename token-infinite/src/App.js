e96bee8e0fd2e273231b6fd598d659fc11236322
22tushar <gehlottushar18@gmal.com>
2022-02-12 21:33:34 +0530

    update:api models and routes

--- token-infinite/src/App.js
+++ token-infinite/src/App.js
@@ -1,18 +1,30 @@
 
-import './App.css';
-import Login from './pages/login/Login';
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Homepage from "./pages/homepage/Homepage";
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from 'react-router-dom'
+import { useContext } from "react";
+import { Context } from "./context/Context";
+import Footer from "./components/footer/Footer";
+
+
+
 function App() {
+  const {user} = useContext(Context);
   return (
     <Router>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
+
+      {/* Gestion des routes  */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Register />} />
+      </Routes>
+
+    </Router>
   );
 }
 
--- token-infinite/src/pages/login/Login.jsx
+++ token-infinite/src/pages/login/Login.jsx
@@ -1,28 +1,69 @@
-
+import axios from "axios";
+import { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom"
+import { Context } from "../../context/Context";
+import "./login.css"
+import loginimage from '../../images/loginimage.png'
+
 
 export default function Login() {
-   
+    const userRef= useRef()
+    const passwordRef = useRef()
+    const { dispatch, isFetching} = useContext(Context)
+
+    const [error, setError] = useState(false)
+
+    // HANDLE SUBMIT
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        setError(false)
+        dispatch({type:"LOGIN_START"});
+        
+        await axios.post("/auth/login",{
+            username:userRef.current.value,
+            password:passwordRef.current.value,
+        })
+        .then((res)=>{
+            dispatch({type:"LOGIN_SUCCESS", payload:res.data});
+        })
+        .catch((err)=>{
+            dispatch({type:"LOGIN_FAILURE"});
+            setError(true)
+        })
+    }
+
+    
     
     return (
         <div className="login">
-            <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input type="text" className="loginInput" placeholder="Enter your username..." 
-                 autoFocus/>
 
-                <label>Password</label>
-                <input type="password" className="loginInput" placeholder="Enter your password..."
-                />
+            <div class="container">
+                <div class="row">
+                    {/* <div class="col">
 
-                <button className="loginButton" type="submit">Login</button>
-            </form>
+                        <img src={loginimage} />
 
-            <button className="loginRegisterButton">
-                <Link to="/register" className="link">Register</Link>    
-            </button>
-    
+                    </div> */}
+                    <div class="col">
+                        
+                        <span className="loginTitle">Login</span>
+                        <form className="loginForm" onSubmit={handleSubmit}>
+                            <label>Username</label>
+                            <input type="text" className="loginInput" placeholder="Enter your username..." 
+                            ref={userRef} autoFocus/>
+
+                            <label>Password</label>
+                            <input type="password" className="loginInput" placeholder="Enter your password..."
+                            ref={passwordRef} />
+
+                            <button className="loginButton" type="submit" disabled={isFetching}>Login</button>
+                        </form>
+                        {error? (<span style={{color:"red", marginTop:"15px"}}> Something went wrong! </span>):(null) }
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
