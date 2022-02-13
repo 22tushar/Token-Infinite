a1098f88418605298fd8cc056d1a7c1eda1c1b83
ishanmjr <mujumdarishan@gmail.com>
2022-02-13 16:30:49 +0530

    Updated homepage and navbar

--- token-infinite/src/components/header/Header.jsx
+++ token-infinite/src/components/header/Header.jsx
@@ -1,23 +1,31 @@
 import './header.css'
-import ImageHeader from '../../images/undraw_voice_control_ofo1 1 (1).png'
+import ImageHeader from '../../images/undraw_voice_control_ofo1.png'
 
 
 export default function Header() {
     return (
-        <div className="header">
-            <div className="Title">
-                Virtual <br />
-                Placement Cells <br/>
-            <a href='/login'><button className="btn btn-outline-danger">Login</button></a>
-            <a href='/register'><button className="btn btn-outline-danger">Register</button></a>  
-            </div>
+        <>
 
-            <div  className="img" >
-             <img
-            className="headerImg" 
-            src={ImageHeader}
-            alt="" />   
-             </div>     
+        <div className="header container">
+            <div className="row justify-content-between">
+                    <div className="Title col-md-5">
+                        Virtual <br />
+                        Placement Cells <br/>
+                    <a href='/login'><button className="btn btn-outline-danger">Login</button></a>
+                    <a href='/register'><button className="btn btn-outline-danger">Register</button></a>
+                    </div>
+             <div  className="img col-md-7">
+                    <img
+                    className="headerImg pull-right" 
+                    src={ImageHeader}
+                    alt="" />
+             </div>
+            </div>
         </div>
+
+
+
+        </>
+
     )
 }
